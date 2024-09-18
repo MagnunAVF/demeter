7b0e26f5a1d1261724028eb26b22fd1e3683cb3c
MagnunAVF <magnunavf@gmail.com>
2024-09-17 21:54:01 -0300

    feat: create Farm model

---
+++ api/test/core/farm/models/farm.test.ts
@@ -0,0 +1,38 @@
+import Crop from '../../../../src/core/crop/models/Crop'
+import Farm from '../../../../src/core/farm/models/Farm'
+
+describe('Farm Class', () => {
+  let name: string, city: string, state: string
+
+  beforeAll(() => {
+    name = 'My farm'
+    city = 'Cerro Largo'
+    state = 'RS'
+  })
+
+  it('should create a Farm instance with valid arguments', () => {
+    const crops = [new Crop('corn'), new Crop('soybeans')]
+    const farm = new Farm(name, city, state, 100, 40, 30, crops)
+
+    expect(farm).toBeInstanceOf(Farm)
+    expect(farm.name).toBe(name)
+    expect(farm.city).toBe(city)
+    expect(farm.state).toBe(state)
+    expect(farm.totalArea).toBe(100)
+    expect(farm.arableArea).toBe(40)
+    expect(farm.vegetationArea).toBe(30)
+    expect(farm.crops).toEqual(crops)
+  })
+
+  it('should throw an error if arable and vegetation areas exceed total area', () => {
+    expect(() => {
+      new Farm(name, city, state, 100, 60, 50, [])
+    }).toThrow('The sum of arable and vegetation area cannot exceed total area')
+  })
+
+  it('should not throw an error if arable and vegetation areas are equal to total area', () => {
+    expect(() => {
+      new Farm(name, city, state, 100, 50, 50, [])
+    }).not.toThrow()
+  })
+})
