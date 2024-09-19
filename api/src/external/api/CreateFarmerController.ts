ae53108669928005aaf578e7c97b8e34f6fe7e3f
MagnunAVF <magnunavf@gmail.com>
2024-09-19 18:03:15 -0300

    feat: implement create farmer route

---
+++ api/src/external/api/CreateFarmerController.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Express } from 'express'
+import CreateFarmer from '../../core/useCases/CreateFarmer'
+import CreateFarmerDto from './dtos/CreateFarmerDto'
+import Crop from '../../core/models/Crop'
+import Farm from '../../core/models/Farm'
+import Farmer from '../../core/models/Farmer'
+import CpfAndCnpjValidator from '../validators/CpfAndCnpjValidator'
+
+class CreateFarmerController {
+  private transformDtoToModel(farmerData: CreateFarmerDto): Farmer {
+    const crops = farmerData.farm.crops.map((cropName) => new Crop(cropName))
+    const { name, city, state, totalArea, arableArea, vegetationArea } =
+      farmerData.farm
+    const farm = new Farm(
+      name,
+      city,
+      state,
+      totalArea,
+      arableArea,
+      vegetationArea,
+      crops
+    )
+    const farmer = new Farmer(
+      farmerData.name,
+      farmerData.document,
+      farm,
+      new CpfAndCnpjValidator()
+    )
+
+    return farmer
+  }
+
+  constructor(server: Express, useCase: CreateFarmer) {
+    server.post('/farmers', async (req, resp) => {
+      try {
+        const farmerData: CreateFarmerDto = req.body as CreateFarmerDto
+        await useCase.execute(this.transformDtoToModel(farmerData))
+        console.log(`[INFO] Farmer Created! ${JSON.stringify(farmerData)}`)
+
+        resp.status(201).send({})
+      } catch (error: any) {
+        console.log(`Error Creating Farm: ${error}`)
+
+        resp.status(500).send('Internal Server Error')
+      }
+    })
+  }
+}
+
+export default CreateFarmerController
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,9 @@
 import express from 'express'
+import PgFarmerRepository from './external/repository/PgFarmerRepository'
+import CpfAndCnpjValidator from './external/validators/CpfAndCnpjValidator'
+import CreateFarmer from './core/useCases/CreateFarmer'
+import Uuid from './external/shared/Uuid'
+import CreateFarmerController from './external/api/CreateFarmerController'
 
 const app = express()
 const PORT = 3000
@@ -9,6 +14,21 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', async (req, res) => {
   res.status(200).send({ msg: 'OK' })
 })
+
+// adapters
+const idGenerator = new Uuid()
+const documentValidator = new CpfAndCnpjValidator()
+const farmerRepository = new PgFarmerRepository(documentValidator)
+
+// use cases
+const createFarmer = new CreateFarmer(
+  farmerRepository,
+  documentValidator,
+  idGenerator
+)
+
+// routes and controllers
+new CreateFarmerController(app, createFarmer)
 
 app.listen(PORT, () => {
   console.log(`Running in http://localhost:${PORT}!`)
